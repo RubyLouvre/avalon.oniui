85cd312212fe055f9f55b1595cd9f0b6a0f5650d
qincheng <qincheng@unitedstack.com>
2013-11-18 22:20:22 +0800

    Remove user/pass from ajaxLocation

--- public/mmRequest.js
+++ public/mmRequest.js
@@ -13,7 +13,7 @@ define("mmRequest", this.FormData ? ["avalon", "mmDeferred"] : ["avalon, mmReque
             rlocalProtocol = /^(?:about|app|app-storage|.+-extension|file|res|widget):$/,
             rnoContent = /^(?:GET|HEAD)$/,
             rquery = /\?/,
-            rurl = /^([\w.+-]+:)(?:\/\/([^\/?#:]*)(?::(\d+)|)|)/,
+			rurl = /^([\w.+-]+:)(?:\/\/(?:[^\/?#]*@|)([^\/?#:]*)(?::(\d+)|)|)/,
             //在IE下如果重置了document.domain，直接访问window.location会抛错，但用document.URL就ok了
             //http://www.cnblogs.com/WuQiang/archive/2012/09/21/2697474.html
             curl = DOC.URL,

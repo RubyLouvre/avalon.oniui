bf1b1a4e723271eb42087cabc1c6482f87ab281b
qincheng <qincheng@unitedstack.com>
2014-06-11 22:59:48 +0800

    fix IE6 样式BUG

--- avalon.js
+++ avalon.js
@@ -2593,9 +2593,9 @@
             val = val == null ? "" : val //不在页面上显示undefined null
             var node = data.node
             if (data.nodeType === 3) { //绑定在文本节点上
-                if (node && node.parentNode) {//IE对游离于DOM树外的节点赋值会报错
+                try{//IE对游离于DOM树外的节点赋值会报错
                     node.data = val
-                }
+                }catch(e){}
             } else { //绑定在特性节点上
                 if (!elem) {
                     elem = data.element = node.parentNode

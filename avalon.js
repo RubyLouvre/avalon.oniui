b29833745173eb29b380101fb4b0ebadd6f8abb0
RubyLouvre <cheng19840218@gmail.com>
2014-06-19 18:38:12 +0800

    update avalon

--- avalon.js
+++ avalon.js
@@ -5,7 +5,7 @@
  http://weibo.com/jslouvre/
  
  Released under the MIT license
- avalon 1.3.1 2014.6.9
+ avalon 1.3.1 2014.6.12
  ==================================================*/
 (function(DOC) {
     var prefix = "ms-"
@@ -37,7 +37,7 @@
 
     function noop() {
     }
-    
+
     function log(a) {
         if (window.console && avalon.config.debug) {
             console.log(W3C ? a : a + "")
@@ -1848,7 +1848,7 @@
     //IE67下，在循环绑定中，一个节点如果是通过cloneNode得到，自定义属性的specified为false，无法进入里面的分支，
     //但如果我们去掉scanAttr中的attr.specified检测，一个元素会有80+个特性节点（因为它不区分固有属性与自定义属性），很容易卡死页面
     if (!"1" [0]) {
-        var cacheAttr = createCache(512)
+        var cacheAttrs = createCache(512)
         var rattrs = /\s+(ms-[^=\s]+)(?:=("[^"]*"|'[^']*'|[^\s>]+))?/g,
                 rquote = /^['"]/,
                 rtag = /<\w+\b(?:(["'])[^"]*?(\1)|[^>])*>/i,
@@ -1861,8 +1861,8 @@
             var attributes = [],
                     match,
                     k, v;
-            if (cacheAttr[str]) {
-                return cacheAttr[str]
+            if (cacheAttrs[str]) {
+                return cacheAttrs[str]
             }
             while (k = rattrs.exec(str)) {
                 v = k[2]
@@ -1878,7 +1878,7 @@
                 }
                 attributes.push(binding)
             }
-            return cacheAttr(str, attributes)
+            return cacheAttrs(str, attributes)
         }
     }
 
@@ -2037,7 +2037,7 @@
         }
         return cache;
     }
-    var cacheExpr = createCache(256)
+    var cacheExprs = createCache(256)
     //取得求值函数及其传参
     var rduplex = /\w\[.*\]|\w\.\w/
     var rproxy = /(\$proxy\$[a-z]+)\d+$/
@@ -2072,7 +2072,7 @@
         }
         data.args = args
         //---------------cache----------------
-        var fn = cacheExpr[exprId] //直接从缓存，免得重复生成
+        var fn = cacheExprs[exprId] //直接从缓存，免得重复生成
         if (fn) {
             data.evaluator = fn
             return
@@ -2110,7 +2110,7 @@
                     "= vvv;\n} "
             try {
                 fn = Function.apply(noop, names.concat(_body))
-                data.evaluator = cacheExpr(exprId, fn)
+                data.evaluator = cacheExprs(exprId, fn)
             } catch (e) {
                 log("debug: parse error," + e.message)
             }
@@ -2130,7 +2130,7 @@
         }
         try {
             fn = Function.apply(noop, names.concat("'use strict';\n" + prefix + code))
-            data.evaluator = cacheExpr(exprId, fn)
+            data.evaluator = cacheExprs(exprId, fn)
         } catch (e) {
             log("debug: parse error," + e.message)
         } finally {
@@ -2229,7 +2229,7 @@
             }
         }
     }
-    var includeContents = {}
+    var cacheTmpls = avalon.templateCache = {}
     var ifSanctuary = DOC.createElement("div")
     ifSanctuary.innerHTML = "a"
     try {
@@ -2280,15 +2280,15 @@
                     })
                 }
                 if (data.param === "src") {
-                    if (includeContents[val]) {
-                        scanTemplate(includeContents[val])
+                    if (cacheTmpls[val]) {
+                        scanTemplate(cacheTmpls[val])
                     } else {
                         var xhr = getXHR()
                         xhr.onreadystatechange = function() {
                             if (xhr.readyState === 4) {
                                 var s = xhr.status
                                 if (s >= 200 && s < 300 || s === 304 || s === 1223) {
-                                    scanTemplate(includeContents[val] = xhr.responseText)
+                                    scanTemplate(cacheTmpls[val] = xhr.responseText)
                                 }
                             }
                         }
@@ -2593,9 +2593,10 @@
             val = val == null ? "" : val //不在页面上显示undefined null
             var node = data.node
             if (data.nodeType === 3) { //绑定在文本节点上
-                try{//IE对游离于DOM树外的节点赋值会报错
+                try {//IE对游离于DOM树外的节点赋值会报错
                     node.data = val
-                }catch(e){}
+                } catch (e) {
+                }
             } else { //绑定在特性节点上
                 if (!elem) {
                     elem = data.element = node.parentNode

e01fefbda21b550a368ce299bf603edf232acaee
RubyLouvre <cheng19840218@gmail.com>
2014-08-05 18:01:01 +0800

    update mmRequest

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
 //https://github.com/zeMirco/express-upload-progress
 app.use(express.static(__dirname + '/public'));
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(app.router);
-app.get('/', function(req, res) {
+//app.use(express.favicon());
+//app.use(express.logger('dev'));
+//app.use(express.bodyParser());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+//http://scotch.io/tutorials/javascript/learn-to-use-the-new-router-in-expressjs-4
+var router = express.Router();
+router.get('/', function(req, res) {
     res.sendfile('index.html');
 });
-app.post("/post", function(req, res) {
+app.use('/', router);
+router.post("/post", function(req, res) {
     console.log(req.body)
 
     if (req.xhr) {
         res.json({msg: '提交成功'});
     }
 });
-app.get("/get", function(req, res) {
+router.get("/get", function(req, res) {
     if (req.xhr) {
         res.send("这是后端返回的数据");
     }
 });
-app.get("/getjson", function(req, res) {
+router.get("/getjson", function(req, res) {
     console.log(req.xhr)
     if (req.xhr) {
         res.send({"msg": 'json', "channel": "ajax"});

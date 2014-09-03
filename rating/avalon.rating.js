da3e74f389a1abe5506ace98d8674329c136be9e
EdwonLim <edwon.lim@gmail.com>
2014-09-03 16:26:00 +0800

    update

--- rating/avalon.rating.js
+++ rating/avalon.rating.js
@@ -1,22 +1,25 @@
 define(["../avalon.getModel", 'text!./avalon.rating.html'],
     function(avalon, sourceHTML) {
 
-    var ratingTemplate = sourceHTML;
+    var ratingTemplate = sourceHTML,
+        getFunc = function(name, vmodels) {
+            var changeVM = avalon.getModel(name, vmodels);
+            return changeVM && changeVM[1][changeVM[0]] || avalon.noop;
+        },
+        canEdit = function(el) {
+            var tagName = el.tagName.toUpperCase();
+            return tagName === 'INPUT' || tagName === 'SELECT';
+        };
 
     var widget = avalon.ui.rating = function(element, data, vmodels) {
         var options = data.ratingOptions,
-            parentNode = element.parentNode,
-            changeVM = avalon.getModel('onSelect', vmodels),
-            onSelect = changeVM && changeVM[1][changeVM[0]] || avalon.noop,
-            value = parseInt(element.value),
+            onSelect = getFunc('onSelect', vmodels),
+            onFloat = getFunc('onFloat', vmodels),
             rating;
-
-        options.value = element.value =
-            (!value && value !== 0) ? options.value : value;
 
         var vmodel = avalon.define(data.ratingId, function(vm) {
 
-            vm.value = vm.fixedValue = element.value;
+            vm.value = vm.floatValue = element.value;
             vm._max = options.max;
             vm._size = options.size;
             vm._selectedImg = options.imgUrls[0];
@@ -24,50 +27,63 @@ define(["../avalon.getModel", 'text!./avalon.rating.html'],
             vm.list = new Array(options.max);
 
             vm.mouseover = function(index) {
-                vm.fixedValue = index + 1;
+                vm.floatValue = index + 1;
             };
 
             vm.select = function(index) {
                 vm.value = index + 1;
-                onSelect.call(null, vm.value,
-                    data.ratingId, avalon(element).data());
             };
 
             vm.mouseout = function() {
-                vm.fixedValue = vm.value;
+                vm.floatValue = vm.value;
+            };
+
+            vm.setByIp = function() {
+                var value = parseInt(element.value);
+                vm.value = vm.floatValue = (!value && value !== 0) ? options.defaultValue : value;
             };
 
             vm.$init = function() {
                 rating = avalon.parseHTML(ratingTemplate).firstChild;
-                parentNode.insertBefore(rating, element);
-                parentNode.insertBefore(element, rating);
-                avalon.scan(rating, [vmodel].concat(vmodels));
-                if (element.tagName.toUpperCase() === 'INPUT') {
-                    avalon.bind(element, 'change', function() {
-                        vm.valve = vm.fixedValue =
-                            parseInt(element.value);
-                    });
+                if (canEdit(element)) {
+                    var parentNode = element.parentNode;
+                    parentNode.insertBefore(rating, element);
+                    parentNode.insertBefore(element, rating);
+                    element.setAttribute("data-duplex-changed", "setByIp");
                     vm.$watch('value', function(v) {
                         element.value = v;
                     });
+                } else {
+                    vm.value = vm.floatValue = options.defaultValue;
+                    element.appendChild(rating);
                 }
+                avalon.scan(rating.parentNode, [vmodel].concat(vmodels));
             };
 
             vm.$remove = function() {
-                parentNode.removeChild(rating);
+                rating.parentNode.removeChild(rating);
                 rating = null;
-                parentNode = null;
             };
+
+            vm.$watch('value', function(v) {
+                onSelect.call(null, v,
+                    data.ratingId, avalon(element).data());
+            });
+
+            vm.$watch('floatValue', function(v) {
+                onFloat.call(null, v,
+                    data.ratingId, avalon(element).data());
+            });
         });
 
         return vmodel;
     };
 
     widget.defaults = {
-        value: 3,
+        defaultValue: 3,
+        imgUrls: ['rating_b.png', 'rating.png'],
         max: 5,
-        size: 20,
-        imgUrls: ['rating_b.png', 'rating.png']
+        size: 20
     };
 
     return avalon;

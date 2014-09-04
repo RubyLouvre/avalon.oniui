cf6cb682a2141d4f0ece3437710f7273a3b00896
shirlyLoveU <shirlyloveflower@gmail.com>
2014-09-04 18:18:25 +0800

    update avalon

--- avalon.js
+++ avalon.js
@@ -1749,9 +1749,6 @@
             } else {
                 try {
                     var c = data.type === "on" ? data : fn.apply(0, data.args)
-                    if (data.type === "include") {
-                        delete Registry[expose];
-                    }
                     data.handler(c, data.element, data)
                 } catch (e) {
                     delete data.evaluator

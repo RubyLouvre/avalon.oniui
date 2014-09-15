5db5b7a38c649934b19a27ac21bc4b437e1a1777
dragonwong <whileking@gmail.com>
2014-09-15 21:41:26 +0800

    insert plupload in to avalon.uploader initially and simply

--- uploader/avalon.uploader.js
+++ uploader/avalon.uploader.js
@@ -4,13 +4,35 @@ define(["avalon", "text!./avalon.uploader.html", "uploader/plupload.dev", "css!.
 		var vmodel = avalon.define(data.uploaderId, function(vm){
 
 			vm.$init = function(){
-				element.outerHTML = sourceHTML;
-				console.log(element);
+
+				// 取 dom
+				var dom_arr = sourceHTML.split("MS_OPTION_COM").map(function(item){
+						return avalon.parseHTML(item).firstChild;
+					}),
+					ul = dom_arr[0],
+					ul_fileList = dom_arr[1],
+					ul_container = dom_arr[2],
+					ul_pick = dom_arr[3],
+					ul_upload = dom_arr[4],
+					ul_console = dom_arr[5],
+					ul_preview = dom_arr[6];
+
+				// 组装
+				ul.appendChild(ul_fileList);
+				ul_container.appendChild(ul_pick);
+				ul_container.appendChild(ul_upload);
+				ul.appendChild(ul_container);
+				ul.appendChild(ul_console);
+				ul.appendChild(ul_preview);
+
+				element.parentNode.replaceChild(ul, element);
+
+				// console.log(element);
 
 				var uploader = new plupload.Uploader({
 					runtimes : 'html5,flash,silverlight,html4',
-					browse_button : 'pickfiles', // you can pass in id...
-					container: document.getElementById('container'), // ... or DOM Element itself
+					browse_button : ul_pick, // you can pass in id...
+					container: ul_container, // ... or DOM Element itself
 					url : 'upload.php',
 					flash_swf_url : 'Moxie.swf',
 					silverlight_xap_url : 'Moxie.xap',
@@ -25,9 +47,9 @@ define(["avalon", "text!./avalon.uploader.html", "uploader/plupload.dev", "css!.
 
 					init: {
 						PostInit: function() {
-							document.getElementById('filelist').innerHTML = '';
+							ul_fileList.innerHTML = '';
 
-							document.getElementById('uploadfiles').onclick = function() {
+							ul_upload.onclick = function() {
 								uploader.start();
 								return false;
 							};
@@ -36,7 +58,7 @@ define(["avalon", "text!./avalon.uploader.html", "uploader/plupload.dev", "css!.
 						FilesAdded: function(up, files) {
 							plupload.each(files, function(file) {
 
-								document.getElementById('filelist').innerHTML += '<div id="' + file.id + '">' + file.name + ' (' + plupload.formatSize(file.size) + ') <b></b></div>';
+								ul_fileList.innerHTML += '<div id="' + file.id + '">' + file.name + ' (' + plupload.formatSize(file.size) + ') <b></b></div>';
 
 								// window.file = file;
 								// console.log(file.getSource());
@@ -44,7 +66,7 @@ define(["avalon", "text!./avalon.uploader.html", "uploader/plupload.dev", "css!.
 								var img = new mOxie.Image();
 
 								img.onload = function() {
-									this.embed(document.getElementById('preview'), {
+									this.embed(ul_preview, {
 										width: 100,
 										height: 100,
 										crop: true
@@ -69,7 +91,7 @@ define(["avalon", "text!./avalon.uploader.html", "uploader/plupload.dev", "css!.
 						},
 
 						Error: function(up, err) {
-							document.getElementById('console').innerHTML += "\nError #" + err.code + ": " + err.message;
+							ul_console.innerHTML += "\nError #" + err.code + ": " + err.message;
 						}
 					}
 				});

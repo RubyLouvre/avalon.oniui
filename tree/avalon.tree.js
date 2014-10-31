1cbafc92d8f595b61231b367b3b1a4b83dee10ea
yang qianjun <qianjun.yang@qunar.com>
2014-10-31 17:12:09 +0800

    finish build tree

--- tree/avalon.tree.js
+++ tree/avalon.tree.js
@@ -5,6 +5,9 @@
 define(["avalon", "text!./avalon.tree.html", "text!./avalon.tree.leaf.html", "text!./avalon.tree.parent.html",  "text!./avalon.tree.nodes.html", "css!./avalon.tree.css", "css!../chameleon/oniui-common.css"], function(avalon, template, leafTemplate, parentTemplate, nodesTemplate) {
 
     //  tool functions
+    function g(id) {
+        return document.getElementById(id)
+    }
 
     //  树状数据的标准化，mvvm的痛
     function dataFormator(arr) {
@@ -12,27 +15,65 @@ define(["avalon", "text!./avalon.tree.html", "text!./avalon.tree.leaf.html", "te
             itemFormator(item)
         })
     }
+    /**
+      * 格式化数据，补全字段
+      */
     function itemFormator(item) {
-        item.isParent = !!item.isParent
+        item.isParent = itemIsParent(item)
+        item.pId = item.pId || 0
         if(item.isParent) {
             item.open = !!item.open
             item.children = item.children || []
             if(item.children.length) dataFormator(item.children)
         }
+        return item
+    }
+    function itemIsParent(item) {
+        return !!item.isParent || !!item.open || !!item.children
     }
     /**  将简单的数组结构数据转换成树状结构
       *  注如果是一个没有子节点的父节点必须加isParent = true，open属性只有父节点有必要有
       *  input array like [
       *      {id: 1, pId: 0, name: xxx, open: boolean, others},// parent node
-      *      {id: 11, pId: 1, name: xxx, others}// 子几点
+      *      {id: 11, pId: 1, name: xxx, others}// 子节点
       *  ]
       */
     function simpleDataToTreeData(arr) {
         if(!arr.length) return []
-        var tree = [], stack = [], last = itemFormator(arr[0]), len = arr.length
-        for(var i = 1; i < len; i++) {
-            
+        var prev, tree = [], stack = [], tar, now
+        for(var i = 0, len = arr.length; i < len; i++) {
+            now = itemFormator(arr[i])
+            // 前一个节点是直属父节点
+            if(prev && prev.id === now.pId) {
+                // 标记父节点
+                prev.isParent = true 
+                itemFormator(prev)
+                // 防止重复压入堆栈
+                if(!tar || tar !== prev) {
+                    stack.push(prev)
+                    tar = prev
+                }
+                tar.children.push(now)
+            // 当前节点是一个父节点或者没有出现过父节点或者出现的父节点非自己的父节点
+            } else if(now.isParent || !tar || tar.id !== now.pId) {
+                // 出栈知道找到自己的父节点或者栈空
+                while(tar && (now.pId !== tar.id)) {
+                    stack.pop()
+                    tar = stack[stack.length - 1]
+                }
+                (tar && tar.children || tree).push(now)
+                // 明确已知自己是一个父节点，压入栈中
+                if(now.isParent) {
+                    stack.push(now)
+                    tar = now
+                }
+            // 非父节点以及未确认是否父节点
+            } else {
+                (tar && tar.children || tree).push(now)
+            }
+            prev = now
         }
+        return tree
     }
 
     var widget = avalon.ui.tree = function(element, data, vmodels) {
@@ -78,9 +119,14 @@ define(["avalon", "text!./avalon.tree.html", "text!./avalon.tree.leaf.html", "te
             vm.hasRemoveBtn = function(leaf) {
 
             }
-            vm.hasChildren = function(leaf) {
+            vm.hasChildren = function(leaf, visible) {
                 // 有有效子节点
-                if(leaf.children && leaf.children.length && vm.hasClassOpen(leaf)) return true;
+                var renderStatus = leaf.children && leaf.children.length && vm.hasClassOpen(leaf)
+                if(visible) {
+                    return renderStatus
+                } else {
+                    return renderStatus || g("c" + leaf.$id)
+                }
             }
 
             vm.loadLeafTemplate = function(leaf) {
@@ -93,7 +139,17 @@ define(["avalon", "text!./avalon.tree.html", "text!./avalon.tree.leaf.html", "te
                 return vm.nodesTemplate.replace(/leaf=\"children\"/g, "leaf=\"leaf.children\"")
             }
 
-            //@method apiName(argx) description
+            vm.timeStamp = function() {
+                return Date.now()
+            }
+            //@method exChangeNodes(a, b) 交换a、b节点的位置，同步到dom
+            vm.exChangeNodes = function(a, b, leaf) {
+                // 交换数据
+                var tmp = vm.children[a]
+                vm.children[a] = vm.children[b]
+                vm.children[b] = tmp
+                // 交换节点
+            }
 
         })
       

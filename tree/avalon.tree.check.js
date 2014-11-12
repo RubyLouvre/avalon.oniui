d7f637792076a85c47f5d6180bca7af879936567
yang qianjun <qianjun.yang@qunar.com>
2014-11-12 18:47:20 +0800

    almost finish tree

--- tree/avalon.tree.check.js
+++ tree/avalon.tree.check.js
@@ -3,9 +3,9 @@ define(["avalon", "./avalon.tree", "text!./avalon.tree.check.html"], function(av
 	function g(id) {return document.getElementById(id)}
 	var undefine = void 0
 	// 排除辅助字段
-	avalon.ui.tree.leafIgnoreField.push("chkFocus")
+	avalon.ui.tree.leafIgnoreField.push("chkFocus", "chkTotal")
 	avalon.ui.tree.AddExtention(
-		["check", "data"],
+		["check", "data", "callback"],
 		{
 			check: {
 				enable: false,
@@ -28,6 +28,18 @@ define(["avalon", "./avalon.tree", "text!./avalon.tree.check.html"], function(av
                 	chkFocus: "chkFocus",
                 	chkTotal: ""
 				}
+			},
+			callback: {
+				beforeCheck: avalon.noop,
+				onCheck: avalon.noop,
+				beforeCheckRelated: function(arg) {
+					if(arg && arg.vm && arg.vm.check && !arg.vm.check.enable || arg.e && arg.e.expr === false) {
+						avalon.log("check is not enable")
+						return false
+					}
+					avalon.log("check is enable")
+					return true
+				}
 			}
 		},
 		function(vm, vmodels) {
@@ -38,14 +50,18 @@ define(["avalon", "./avalon.tree", "text!./avalon.tree.check.html"], function(av
 				chkBlur: function(arg) {
 					if(arg.leaf) arg.leaf.chkFocus = false
 				},
+				checkEnable: function() {
+					return !!vm.check.enable
+				},
 				computeCHKClass: function(leaf) {
 					var type = vm.getCheckType()
-					return type + "_" + !!leaf.checked + "_" + (leaf.halfCheck ? "part" : "full") + (leaf.chkFocus ? "_focus" : "")
+					return type + "_" + !!leaf.checked + "_" + (leaf.halfCheck ? "part" : leaf.chkDisabled ? "disable" : "full") + (leaf.chkFocus ? "_focus" : "")
 				},
 				getCheckType: function() {
 					return vm.check.chkStyle === "radio" ? "radio" : "checkbox"
 				},
 				checkNode: function(leaf, checked, checkTypeFlag, callbackFlag) {
+					if(!vm.checkEnable() || leaf.chkDisabled) return
 					vm.excute("check", {
 						cancelCallback: !callbackFlag
 					}, leaf, function() {
@@ -54,7 +70,7 @@ define(["avalon", "./avalon.tree", "text!./avalon.tree.check.html"], function(av
 					})
 				},
 				checkAllNodes: function(checked, leaf) {
-					if(!vm.check.enable && vm.check.chkStyle !== "checkbox") return
+					if(!vm.checkEnable() && vm.check.chkStyle !== "checkbox") return
 					vm.visitor(leaf, function(node) {
 						if(!node.nocheck && !node.chkDisabled) {
 							node.checked = !!checked
@@ -74,7 +90,37 @@ define(["avalon", "./avalon.tree", "text!./avalon.tree.check.html"], function(av
 
 				},
 				setChkDisabled: function(leaf, disabled, inheritParent, inheritChildren) {
-
+					if(vm.checkEnable()) {
+						disabled = !!disabled
+						leaf.chkDisabled = disabled
+						// 操作子节点
+						if(inheritChildren) {
+							vm.visitor(leaf, function(node) {
+								if(node.nocheck) return	
+								node.chkDisabled = disabled
+							}, function(res, node) {
+								// 终止这个节点，以及其子节点
+								return node.nocheck
+							}, [])
+						}
+						// 影响父节点
+						if(inheritChildren && leaf && leaf.$parentLeaf) {
+							// 向上溯源
+							vm.cVisitor(leaf, function(node) {
+								var par = node.$parentLeaf
+								if(!par) return
+								var disabledCount = 0,
+									canDisabledCount = 0
+								// 计算有多少子节点的disable情况
+								vm.brotherVisitor(node, function(node) {
+									if(node.nocheck) return
+									canDisabledCount++
+									if(node.chkDisabled) disabledCount++
+								})
+								par.chkDisabled = disabled ? disabledCount >= canDisabledCount : disabledCount >= canDisabledCount
+							})
+						}
+					}
 				}
 			})
 		}, [], {
@@ -97,9 +143,11 @@ define(["avalon", "./avalon.tree", "text!./avalon.tree.check.html"], function(av
 			vmodel.$watch("e:check", function(arg) {
 				var leaf = arg.leaf,
 					vm = arg.vm,
-					chk = vm.check,
-					chkStyle = chk.chkStyle,
-					radioType = chk.radioType
+					chk = vm.check
+				if(!chk.enable) return
+				var	chkStyle = chk.chkStyle,
+					radioType = chk.radioType,
+					chkboxType = chk.chkboxType
 
 				if(chk.chkStyle === "radio") {
 					if(leaf.checked) {
@@ -115,8 +163,30 @@ define(["avalon", "./avalon.tree", "text!./avalon.tree.check.html"], function(av
 							}, [])
 						}
 					}
+				// only for checkbox
+				} else {
+					// 关联效果
+					var bool = !!leaf.checked
+					chkboxType =  bool ? chkboxType.Y : chkboxType.N
+					if(checkbox) {
+						// 向上关联
+						if(chkboxType.indexOf("p") > -1) {
+							vmodel.brotherVisitor(leaf, function(node) {
+								if(node.nocheck || node.chkDisabled) return
+							}, function(res, node, par) {
+								return par.nocheck || par.chkDisabled
+							})
+						}
+						// 向下关联
+						if(chkboxType.indexOf("s") > -1) {
+							vmodel.visitor(leaf, function(node) {
+								// 级联不进入回调
+								if(node.nocheck || node.chkDisabled) return
+								node.checked = bool
+							}, undefine, [])
+						}
+					}
 				}
-				// 关联效果
 			})
 		})
 })

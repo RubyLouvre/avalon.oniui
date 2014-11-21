ec1f6bb969c6be5b3d2fc9f2b62184598dc5e5b7
RubyLouvre <cheng19840218@gmail.com>
2014-11-21 14:22:54 +0800

    update

--- validation/avalon.validation.old.js
+++ validation/avalon.validation.old.js
@@ -233,16 +233,34 @@ define(["../promise/avalon.promise"], function(avalon) {
         //中国大陆
         cn: /^(?:0?1)[3458]\d{9}$/,
         //中国香港
-        hk: /^(?:0?[1569])(?:\d{7}|\d{8}|\d{12})$/,
+     //   hk: /^(?:0?[1569])(?:\d{7}|\d{8}|\d{12})$/,
         //澳门
-        macao: /^6\d{7}$/,
+       // macao: /^6\d{7}$/,
         //台湾
-        tw: /^(?:0?[679])(?:\d{7}|\d{8}|\d{10})$//*,
+      //  tw: /^(?:0?[679])(?:\d{7}|\d{8}|\d{10})$//*,
          //韩国
-         kr:/^(?:0?[17])(?:\d{9}|\d{8})$/,
+       //  kr:/^(?:0?[17])(?:\d{9}|\d{8})$/,
          //日本
-         jp:/^(?:0?[789])(?:\d{9}|\d{8})$/*/
+        // jp:/^(?:0?[789])(?:\d{9}|\d{8})$/*/
     }
+    /*
+     * http://login.sdo.com/sdo/PRes/4in1_2/js/login.js
+     * function isPhone(val){
+    var gvPhoneRegExpress=new Array();
+    gvPhoneRegExpress.push(/^14[57]\d{8}$/);
+    gvPhoneRegExpress.push(/^15[012356789]\d{8}$/);
+    gvPhoneRegExpress.push(/^13[0-9]\d{8}$/);
+    gvPhoneRegExpress.push(/^18[012456789]\d{8}$/);
+    var lvCellphoneIsOk=false;
+    for (var i=0;i<gvPhoneRegExpress.length;i++){
+        if(gvPhoneRegExpress[i].test(val)){
+            lvCellphoneIsOk=true;
+            break;
+        }
+    }
+    return lvCellphoneIsOk;
+}
+     */
     avalon.mix(avalon.duplexHooks, {
         trim: {
             get: function(value, data) {

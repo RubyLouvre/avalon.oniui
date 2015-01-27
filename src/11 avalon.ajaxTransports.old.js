01a56b57146a902ab0868348f1cf291e5068e554
ilife5 <ilife5@qq.com>
2015-01-27 14:16:24 +0800

    去掉调试代码

--- src/11 avalon.ajaxTransports.old.js
+++ src/11 avalon.ajaxTransports.old.js
@@ -10,12 +10,10 @@ var transports = avalon.ajaxTransports = {
             if (this.mimeType && transport.overrideMimeType) {
                 transport.overrideMimeType(this.mimeType)
             }
-            alert(1)
             //如果transport中没有withCredentials，直接设置会报错
             if (opts.crossDomain && "withCredentials" in transport) {
                 transport.withCredentials = true
             }
-            alert(2)
             this.requestHeaders["X-Requested-With"] = "XMLHttpRequest"
             for (var i in this.requestHeaders) {
                 transport.setRequestHeader(i, this.requestHeaders[i] + "")

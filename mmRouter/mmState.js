a2f762ae35d1c35d5f2553850b424649dbcbc493
yang qianjun <qianjun.yang@qunar.com>
2015-04-02 15:14:15 +0800

    fix module name bug

--- mmRouter/mmState.js
+++ mmRouter/mmState.js
@@ -1,4 +1,4 @@
-define("mmState", ["../mmPromise/mmPromise", "mmRouter/mmRouter"], function() {
+define("mmState", ["../mmPromise/mmPromise", "./mmRouter"], function() {
 //重写mmRouter中的route方法     
     avalon.router.route = function(method, path, query, options) {
         path = path.trim()

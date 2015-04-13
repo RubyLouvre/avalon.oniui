c2ca708ae23b5c63f0c6ea2e0a619a28b4cdc617
Roscoe <heiwu805@hotmail.com>
2015-04-13 17:36:24 +0800

    添加图片链接配置

--- carousel/avalon.carousel.js
+++ carousel/avalon.carousel.js
@@ -9,7 +9,7 @@
 
 define(["avalon", "text!./avalon.carousel.html", "css!./avalon.carousel.css", "css!../chameleon/oniui-common.css"], function(avalon, template) {
 	//获取当前JS绝对路径
-	var path = "",
+	var path,
 		t=document.getElementsByTagName("SCRIPT");
 	for(var i in t){
 		if(t[i].outerHTML && t[i].outerHTML.indexOf("avalon.carousel.js") !== -1){
@@ -44,38 +44,30 @@ define(["avalon", "text!./avalon.carousel.html", "css!./avalon.carousel.css", "c
 	}
 
 	var widget = avalon.ui.carousel = function(element, data, vmodels) {
-		var options = data.carouselOptions,
-			inited;
+		var options = data.carouselOptions
+		options.template = options.getTemplate(template, options)
 
-		template = template.replace("MS_OPTION_CONTENT", options.content);
-		
-		options.template = options.getTemplate(template, options);
+		var vmodel = avalon.define(data.carouselId, function(vm) {
+			avalon.mix(vm, options)
+			vm.widgetElement = element
+			vm.picNum = vm.pictures.length + 1 //图片数量（包括复制到末尾的第一个元素）
+			vm.pictureOpacity = {}
+			vm.itemPosition = "relative" //默认slide effect下结构
+			vm.panelPosition = "absolute" //默认slide effect下结构
+			vm.selections = avalon.range(vm.pictures.length) //圆形选择的数据数组（不包括复制到末尾的第一个元素）
+			vm.currentIndex = 0 // 圆形选择的index
+			vm.selectionWrapOffset = -vm.pictures.length * 20 / 2 //圆形选择CSS位置修正
+			vm.panelOffsetX = 0 //长panel的X方向偏移，正向移动（右）时减小，反向移动（左）时增大
+			vm.arrowVisible = false //箭头是否可见
+			vm.arrowLeftBg = vm.arrowLeftNormalSrc !== "" ? "url("+vm.arrowLeftNormalSrc+")" : ""
+			vm.arrowRightBg = vm.arrowRightNormalSrc !== "" ? "url("+vm.arrowRightNormalSrc+")" : ""
+			vm.imgNodeArr = new Array(vm.picNum)
+			vm.lazyloading = options.lazyload
+			vm.placeholderImg = path + "/images/loading.gif"
+			vm.$skipArray = ["widgetElement", "template", "selectionWrapOffset"]
 
-		// 动画参数
-		var animated = false,				//动画正在进行
-			duringTime = options.during / 10,	//补间动画的时间长度
-			lastIndex;						//上一张图片index
-
-		var hoverIndex = 0,
-			timer = null //轮播计时器
-
-		var vm = {
-			$id: data.carouselId,
-			$skipArray: ["widgetElement", "template", "selectionWrapOffset"],
-			widgetElement: element,
-			picNum: options.pictures.length + 1, //图片数量（包括复制到末尾的第一个元素）
-			pictureOpacity: {},
-			itemPosition: "relative", //默认slide effect下结构
-			panelPosition: "absolute", //默认slide effect下结构
-			selections: avalon.range(options.pictures.length), //圆形选择的数据数组（不包括复制到末尾的第一个元素）
-			currentIndex: 0, // 圆形选择的index
-			selectionWrapOffset: -options.pictures.length * 20 / 2, //圆形选择CSS位置修正
-			panelOffsetX: 0, //长panel的X方向偏移，正向移动（右）时减小，反向移动（左）时增大
-			arrowVisible: false, //箭头是否可见
-			arrowLeftBg: options.arrowLeftNormalSrc !== "" ? "url(" + options.arrowLeftNormalSrc + ")" : "",
-			arrowRightBg: options.arrowRightNormalSrc !== "" ? "url(" + options.arrowRightNormalSrc + ")" : "",
-
-			$init: function(continueScan) {
+			var inited
+			vm.$init = function(continueScan) {
 				if (inited) return
 				inited = true
 				var pageHTML = options.template
@@ -83,10 +75,10 @@ define(["avalon", "text!./avalon.carousel.html", "css!./avalon.carousel.css", "c
 				element.innerHTML = pageHTML
 				element.style.display = "block"
 
-				if (vmodel.adaptiveWidth || vmodel.pictureWidth === "100%") { //自动填充外围容器宽度
-					vmodel.pictureWidth = element.offsetWidth
+				if (vm.adaptiveWidth || vm.pictureWidth === "100%") { //自动填充外围容器宽度
+					vm.pictureWidth = element.offsetWidth
 				}
-				if (vmodel.adaptiveHeight || vmodel.pictureHeight === "100%") { //自动填充外围容器高度
+				if (vm.adaptiveHeight || vm.pictureHeight === "100%") { //自动填充外围容器高度
 					element.style.height = "100%"
 					var children = element.children
 					for (var i = 0, len = children.length; i < len; i++) {
@@ -96,19 +88,37 @@ define(["avalon", "text!./avalon.carousel.html", "css!./avalon.carousel.css", "c
 					}
 				}
 
-				//预加载图片
-				var images = vmodel.pictures,
-					icons = [];
+				// 预加载图片
+				var originPictures = vm.pictures.slice(),
+					placeholderPics = []
+				for (var i = 0; i < vm.picNum; i++) {
+					var imgPreload = new Image()
 
-				for (var i = 0; i < images.length; i++) {
-					var image_preload = new Image()
-					image_preload.src = images[i].src
+					if(vm.lazyload){
+						for(var j = 0; j < vm.picNum; j++){
+							placeholderPics.push(path + "/images/placeholder.png")
+						}
+						vm.pictures = placeholderPics;
+
+						(function(i) {
+							imgPreload.onload = function(e){
+								setTimeout(function(){
+									vm.imgNodeArr[i].setAttribute("src", originPictures[i])
+								},200)
+							}
+						})(i)
+					}
+
+					imgPreload.src = vm.pictures[i].src
 				}
-				icons.push("./images/arrows-left-hover-icon.png","./images/arrows-right-hover-icon.png")
+
+				// 预加载icons
+				var icons = []
+				icons.push("/images/arrows-left-hover-icon.png","/images/arrows-right-hover-icon.png")
 				for (var i = 0; i < icons.length; i++) {
 					icons[i] = path + icons[i]
-					var image_preload = new Image()
-					image_preload.src = icons[i]
+					var icon_preload = new Image()
+					icon_preload.src = icons[i]
 				}
 
 				if(continueScan){
@@ -120,54 +130,61 @@ define(["avalon", "text!./avalon.carousel.html", "css!./avalon.carousel.css", "c
 						options.onInit.call(element, vmodel, options, vmodels)
 					}
 				}
-			},
-			$remove: function() {
+
+			}
+			vm.$remove = function() {
 				element.innerHTML = element.textContent = ""
-			},
-			stopPlay: function() { //hover在组件上时使Arrow显示，停止轮播
-				vmodel.arrowVisible = vmodel.alwaysShowArrow ? true : false
-				if (vmodel.hoverStop && vmodel.autoSlide) {
+			}
+			vm.stopPlay = function() { //hover在组件上时使Arrow显示，停止轮播
+				vm.arrowVisible = vm.alwaysShowArrow ? true : false
+				if (vm.hoverStop && vm.autoSlide) {
 					clearTimeout(timer)
 					timer = null
 				}
-			},
-			restartPlay: function(type) { //hover离开时使Arrow隐藏，重新开始轮播
+			}
+			vm.restartPlay = function(type) { //hover离开时使Arrow隐藏，重新开始轮播
 				if (type === "carousel") {
-					vmodel.arrowVisible = false
+					vm.arrowVisible = false
 				}
-				vmodel.autoPlay(vmodel)
-			},
+				vm.autoPlay()
+			}
+
+			vm.imgOnload = function(e, i){
+				vm.imgNodeArr[i] = e.target
+			}
 
 			//动画参数
-			fadein: 1, //effect为fade时，渐入的图片透明度
-			fadeout: 0, //effect为fade时，渐出的图片透明度
-			
-			animate: function(direct, distance) { //@method animate(direct, distance) 图片滚动，direct为方向（1/-1），distance为距离（>0整数）
+			vm.fadein = 1 //effect为fade时，渐入的图片透明度
+			vm.fadeout = 0 //effect为fade时，渐出的图片透明度
+			var animated = false //动画正在进行
+			var duringTime = vm.during / 10 //补间动画的时间长度
+			var lastIndex //上一张图片index
+			vm.animate = function(direct, distance) { //@method animate(direct, distance) 图片滚动，direct为方向（1/-1），distance为距离（>0整数）
 				if (animated) { //防止动画队列堆积
 					return
 				}
 				distance = distance || 1
-				if (vmodel.effect === "slide") {
+				if (vm.effect === "slide") {
 					//移动准备
-					if (direct === 1 && vmodel.panelOffsetX === -vmodel.pictureWidth * (vmodel.picNum - 1)) { //点击为正方向且panel处于队列末尾，队列先回到0
-						vmodel.panelOffsetX = 0
-					} else if (direct === -1 && vmodel.panelOffsetX === 0) { //点击为负方向且panel处于队列开始，队列先回到末尾
-						vmodel.panelOffsetX = -vmodel.pictureWidth * (vmodel.picNum - 1)
+					if (direct === 1 && vm.panelOffsetX === -vm.pictureWidth * (vm.picNum - 1)) { //点击为正方向且panel处于队列末尾，队列先回到0
+						vm.panelOffsetX = 0
+					} else if (direct === -1 && vm.panelOffsetX === 0) { //点击为负方向且panel处于队列开始，队列先回到末尾
+						vm.panelOffsetX = -vm.pictureWidth * (vm.picNum - 1)
 					}
-					var offset = vmodel.panelOffsetX - vmodel.pictureWidth * direct * distance //设置移动终点位置
+					var offset = vm.panelOffsetX - vm.pictureWidth * direct * distance //设置移动终点位置
 
 					//进行移动
 					var currentTime = 0 //当前时间
-					var startpos = vmodel.panelOffsetX //位置初始值
-					var duringDistance = vmodel.pictureWidth * -direct * distance //位置变化量
+					var startpos = vm.panelOffsetX //位置初始值
+					var duringDistance = vm.pictureWidth * -direct * distance //位置变化量
 					var go = function() {
 						animated = false
-						if ((vmodel.panelOffsetX <= -vmodel.pictureWidth * (vmodel.pictures.length - 1)) && direct > 0) { //队列已到末尾位置，且将要往正方向移动，队列回到0
-							vmodel.panelOffsetX = 0
-						} else if ((vmodel.panelOffsetX >= 0) && direct < 0) { //队列已到开始位置，且将要往反方向移动，队列回到末尾
-							vmodel.panelOffsetX = -vmodel.pictureWidth * (vmodel.picNum - 1)
+						if ((vm.panelOffsetX <= -vm.pictureWidth * (vm.pictures.length - 1)) && direct > 0) { //队列已到末尾位置，且将要往正方向移动，队列回到0
+							vm.panelOffsetX = 0
+						} else if ((vm.panelOffsetX >= 0) && direct < 0) { //队列已到开始位置，且将要往反方向移动，队列回到末尾
+							vm.panelOffsetX = -vm.pictureWidth * (vm.picNum - 1)
 						} else { //队列还未到终点，在移动过程中
-							vmodel.panelOffsetX = Tween[vmodel.easing](currentTime, startpos, duringDistance, duringTime) //移动
+							vm.panelOffsetX = Tween[vm.easing](currentTime, startpos, duringDistance, duringTime) //移动
 							if (currentTime < duringTime) {
 								currentTime += 1
 								requestAnimationFrame(go)
@@ -175,12 +192,12 @@ define(["avalon", "text!./avalon.carousel.html", "css!./avalon.carousel.css", "c
 							}
 						}
 					}
-				} else if (vmodel.effect === "fade") { //effect为fade
+				} else if (vm.effect === "fade") { //effect为fade
 					var currentTime = 0 //当前时间
 					var go = function() {
 						animated = false
-						vmodel.fadein = Tween[vmodel.easing](currentTime, 0, 1, duringTime) //移动
-						vmodel.fadeout = Tween[vmodel.easing](currentTime, 1, -1, duringTime) //移动
+						vm.fadein = Tween[vm.easing](currentTime, 0, 1, duringTime) //移动
+						vm.fadeout = Tween[vm.easing](currentTime, 1, -1, duringTime) //移动
 						if (currentTime < duringTime) {
 							currentTime += 1
 							requestAnimationFrame(go)
@@ -189,103 +206,101 @@ define(["avalon", "text!./avalon.carousel.html", "css!./avalon.carousel.css", "c
 					}
 				} else { //effect为none
 					var go = function() {
-						vmodel.fadein = 1
-						vmodel.fadeout = 0
+						vm.fadein = 1
+						vm.fadeout = 0
 					}
 				}
 				go()
 
 				//更新图片index
-				lastIndex = vmodel.currentIndex //当前图片变为上一张
-				vmodel.currentIndex += 1 * direct * distance
-				if (vmodel.currentIndex > vmodel.selections.length - 1) { //最右端继续+1时回0
-					vmodel.currentIndex = 0
-				} else if (vmodel.currentIndex < 0) { //最左端继续-1时回末尾
-					vmodel.currentIndex = vmodel.selections.length - 1
+				lastIndex = vm.currentIndex //当前图片变为上一张
+				vm.currentIndex += 1 * direct * distance
+				if (vm.currentIndex > vm.selections.length - 1) { //最右端继续+1时回0
+					vm.currentIndex = 0
+				} else if (vm.currentIndex < 0) { //最左端继续-1时回末尾
+					vm.currentIndex = vm.selections.length - 1
 				}
-			},
-			getOpacity: function(index) { //@method getOpacity(index) fade effect 下改变前后图片透明度
-				if (vmodel.effect !== 'slide') {
-					var num = vmodel.fadein + vmodel.fadeout
-					if (index === vmodel.currentIndex) {
-						return vmodel.fadein
+			}
+			vm.getOpacity = function(index) { //@method getOpacity(index) fade effect 下改变前后图片透明度
+				if (vm.effect !== 'slide') {
+					var num = vm.fadein + vm.fadeout
+					if (index === vm.currentIndex) {
+						return vm.fadein
 					} else if (index === lastIndex) {
-						return vmodel.fadeout
+						return vm.fadeout
 					} else {
 						return 0
 					}
 				} else {
 					return 1
 				}
-			},
+			}
 
-			selectPic: function(index, e) { //@method selectPic(index) 通过底部圆形选择图片
+			var hoverIndex = 0;
+			vm.selectPic = function(index, e) { //@method selectPic(index) 通过底部圆形选择图片
 				hoverIndex = index
-				if (e.type === vmodel.eventType || vmodel.eventType === "both") {
-					var distance = vmodel.currentIndex - index
+				if (e.type === vm.eventType || vm.eventType === "both") {
+					var distance = vm.currentIndex - index
 					var direct = distance > 0 ? -1 : 1
 
 					if (e.type === "mouseenter") {
 						setTimeout(function() {
-							vmodel.animate(direct, Math.abs(distance))
+							vm.animate(direct, Math.abs(distance))
 						}, 300) //mouseenter事件设置延时以防止切换时间间隔太小
 					} else {
-						vmodel.animate(direct, Math.abs(distance))
+						vm.animate(direct, Math.abs(distance))
 					}
 
-					if (vmodel.autoSlide) {
+					if (vm.autoSlide) {
 						clearTimeout(timer)
 						timer = null
 					}
+					//修复hover的TAB和select的TAB不一致
+					var fixIndex = setInterval(function(){
+						if(vm.currentIndex !== hoverIndex){
+							var distance = vm.currentIndex - hoverIndex
+							var direct = distance > 0 ? -1 : 1
+							vm.animate(direct, Math.abs(distance))
+						} else{
+							clearInterval(fixIndex)
+						}
+					},800)
 				}
-
-				//修复hover的TAB和select的TAB不一致
-				var fixIndex = setInterval(function(){
-					if(vmodel.currentIndex !== hoverIndex){
-						var distance = vmodel.currentIndex - hoverIndex
-						var direct = distance > 0 ? -1 : 1
-						vmodel.animate(direct, Math.abs(distance))
-					} else{
-						clearInterval(fixIndex)
-					}
-				},800)
-			},
-			arrowHover: function(direction) { //@method arrowHover(direction) 左右箭头hover事件
+			}
+			vm.arrowHover = function(direction) { //@method arrowHover(direction) 左右箭头hover事件
 				if (direction === "left") {
-					vmodel.arrowLeftBg = vmodel.arrowLeftHoverSrc !== "" ? "url(" + vmodel.arrowLeftHoverSrc + ")" : ""
+					vm.arrowLeftBg = vm.arrowLeftHoverSrc !== "" ? "url("+vm.arrowLeftHoverSrc+")" : ""
 				} else {
-					vmodel.arrowRightBg = vmodel.arrowRightHoverSrc !== "" ? "url(" + vmodel.arrowRightHoverSrc + ")" : ""
+					vm.arrowRightBg = vm.arrowRightHoverSrc !== "" ? "url("+vm.arrowRightHoverSrc+")" : ""
 				}
-			},
-			arrowBlur: function(direction) { //@method arrowBlur(direction) 左右箭头blur事件
+			}
+			vm.arrowBlur = function(direction) { //@method arrowBlur(direction) 左右箭头blur事件
 				if (direction === "left") {
-					vmodel.arrowLeftBg = vmodel.arrowLeftNormalSrc !== "" ? "url(" + vmodel.arrowLeftNormalSrc + ")" : ""
+					vm.arrowLeftBg = vm.arrowLeftNormalSrc !== "" ? "url("+vm.arrowLeftNormalSrc+")" : ""
 				} else {
-					vmodel.arrowRightBg = vmodel.arrowRightNormalSrc !== "" ? "url(" + vmodel.arrowRightNormalSrc + ")" : ""
+					vm.arrowRightBg = vm.arrowRightNormalSrc !== "" ? "url("+vm.arrowRightNormalSrc+")" : ""
 				}
-			},
-			autoPlay: function() { //@method autoPlay(vmodel) 自动开始轮播
-				if (timer === null && vmodel.autoSlide) {
+			}
+			var timer = null //轮播计时器
+			vm.autoPlay = function() { //@method autoPlay(vmodel) 自动开始轮播
+				if (timer === null && vm.autoSlide) {
 					function play() {
 						timer = setTimeout(function() {
-							vmodel.animate(1) //正方向移动
+							vm.animate(1) //正方向移动
 							play()
-						}, vmodel.timeout)
+						}, vm.timeout)
 					}
 					play()
 				}
 			}
-		};
-		avalon.mix(vm, options);
-		var vmodel = avalon.define(vm);
-
+		})
 		if (vmodel.effect !== "slide") { //fade 或者 none 模式下的布局
 			vmodel.itemPosition = "absolute"
 			vmodel.panelPosition = "relative"
 		}
 		vmodel.pictures.push( vmodel.pictures[0] )
-	   // vmodel.pictures[vmodel.pictures.length] = vmodel.pictures[0] //将第一个元素加到图片数组末尾形成循环
-		vmodel.autoPlay(vmodel) //自动开始轮播
+		// vmodel.pictures[vmodel.pictures.length] = vmodel.pictures[0] //将第一个元素加到图片数组末尾形成循环
+		vmodel.autoPlay() //自动开始轮播
 
 		return vmodel
 	}
@@ -293,9 +308,9 @@ define(["avalon", "text!./avalon.carousel.html", "css!./avalon.carousel.css", "c
 	widget.vertion = "1.0.1"
 	widget.defaults = {
 		pictures: [], //@config  轮播图片素材
+		links: [], //@config  图片链接
 		pictureWidth: 500, //@config  图片显示宽度
 		pictureHeight: 200, //@config  图片显示高度
-		content: "",	// @config 图片内容模板
 		effect: "slide", //@config  图片切换类型，取值：none:无特效 / fade:渐隐 / slide:滑动
 		easing: "easeInOut", //@config  缓动类型，取值 linear:无缓动效果 / easeIn:在过渡的开始提供缓动效果 / easeOut:在过渡的结尾提供缓动效果 / easeInOut 在过渡的开始和结尾提供缓动效果
 		timeout: 2500, //@config  切换时间间隔
@@ -313,6 +328,7 @@ define(["avalon", "text!./avalon.carousel.html", "css!./avalon.carousel.css", "c
 		arrowRightHoverSrc: "", //@config  右箭头hover状态图标，可不传
 		arrowLeftClass:"", //@config  左右箭头的className，可不传
 		arrowRightClass:"", //@config  左右箭头的className，可不传
+		lazyload: false, //@config  图片进行懒加载
 		onInit: avalon.noop, //@optMethod onInit(vmodel, options, vmodels) 完成初始化之后的回调,call as element's method
 		getTemplate: function(tmpl, opts, tplName) {
 			return tmpl

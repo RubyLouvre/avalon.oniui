96a245b2c93697a895a81771eba3f5badc597cd8
qianjun.yang <qianjun.yang@qunar.com>
2015-08-25 17:52:55 +0800

    保证dropdown设置数据的时候value类型一致

--- dropdown/avalon.dropdown.js
+++ dropdown/avalon.dropdown.js
@@ -322,7 +322,9 @@ define(["avalon",
                     if (!(option = setLabelTitle(vmodel.value))) {
                         vmodel.currentOption = vmodel.data[0].$model;
                         vmodel.activeIndex = 0;
-                        setLabelTitle(vmodel.value = vmodel.data[0].value);
+                        var v = vmodel.data[0].value;
+                        if(vmodel.multiple && !(v instanceof Array)) v = [v]; // 保证类型一致
+                        setLabelTitle(vmodel.value = v);
                     } else {
                         vmodel.activeIndex = vmodel.data.$model.indexOf(option)
                     }

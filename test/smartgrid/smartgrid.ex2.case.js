73336778831b997750ab80ad5b44458b03bce0e1
roscoe <heiwu805@hotmail.com>
2015-09-11 14:12:40 +0800

    test：添加smartgrid test case

---
+++ test/smartgrid/smartgrid.ex2.case.js
@@ -0,0 +1,29 @@
+var expect = chai.expect;
+
+describe('smartgrid ex2', function(){
+
+    it('初始化时渲染8条数据', function(){
+        var clearBtn = $("button:contains('重新渲染数据')")
+        clearBtn.simulate("click")
+
+        var rowsNum = $(".oni-smartgrid-row").length;
+        expect(rowsNum).to.equal(8);
+    });
+
+    it('第一行数据最左部有一个checkbox', function(){
+        var checkbox = $(".oni-smartgrid-row").eq(0).find(".oni-smartgrid-column").eq(0).find("input[type=checkbox]");
+        expect(checkbox.is(":visible")).to.equal(true);
+    });
+
+    it('点击操作列会弹出dropdown menu', function(done){
+        // 等待组件渲染完毕
+        delay(done, function(){
+            var dropdown = $(".oni-smartgrid-row").eq(0).find(".oni-smartgrid-column").eq(2).find(".oni-dropdown");
+            dropdown.simulate("click")
+
+            var dropdownMenu = $(".oni-dropdown-menu").eq(0)
+            expect(dropdownMenu.is(":visible")).to.equal(true);
+        }, 0)
+    });
+});
+

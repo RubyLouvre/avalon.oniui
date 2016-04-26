59f6a387003bedea514de83a2a7d8477d79d730d
yuhao.ju <heiwu805@hotmail.com>
2016-04-26 14:58:54 +0800

    fix(dropdown): get error when set dropdown value out of range

--- dropdown/avalon.dropdown.js
+++ dropdown/avalon.dropdown.js
@@ -175,7 +175,7 @@ define(["avalon",
                                         duplexModel[1][duplexModel[0]] = n
                                         element.value = n
                                     }
-                                    vmodel.currentOption = setLabelTitle(n);
+                                    vmodel.currentOption = setLabelTitle(n) || {};
                                 }
                             }
                             if ((onChange && onChange.call(element, n, o, vmodel, valueStateKeep) !== false) || !onChange) {
@@ -183,7 +183,12 @@ define(["avalon",
                                     duplexModel[1][duplexModel[0]] = n
                                     element.value = n
                                 }
-                                vmodel.currentOption = setLabelTitle(n);
+                                var currentOption = setLabelTitle(n);
+                                if(currentOption === null) {
+                                    currentOption = {};
+                                    vmodel.label = '';
+                                }
+                                vmodel.currentOption = currentOption;
                             }
                         })
                     });
